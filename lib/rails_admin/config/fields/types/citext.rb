9e83dd3bfd2fec0809bd190061790f81c68a5073
Alex Mekelburg <alex@mekware.com>
2019-01-24 13:02:17 -0500

    citext filters too

--- lib/rails_admin/config/fields/types/citext.rb
+++ lib/rails_admin/config/fields/types/citext.rb
@@ -1,7 +1,33 @@
 require 'rails_admin/config/fields/base'
 
-module RailsAdmin::Config::Fields::Types
-  class Citext < RailsAdmin::Config::Fields::Types::String
-    RailsAdmin::Config::Fields::Types::register(:citext, self)
+module RailsAdmin
+  module Config
+    module Fields
+      module Types
+        class Citext < RailsAdmin::Config::Fields::Types::String
+          RailsAdmin::Config::Fields::Types::register(:citext, self)
+        end
+      end
+    end
   end
+    
+  module Adapters
+      module ActiveRecord
+        module CitextStatement
+          private
+
+          def build_statement_for_type
+            if @type == :citext
+              return build_statement_for_string_or_text
+            else
+              super
+            end
+          end
+        end
+
+        class StatementBuilder < RailsAdmin::AbstractModel::StatementBuilder
+          prepend CitextStatement
+        end
+      end
+    end
 end
