19e1a88358cd5132440f2c4b217ded797fd77b41
Alex Mekelburg <alex@mekware.com>
2019-01-24 13:21:45 -0500

    fix ordering

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,7 +1,7 @@
 class NotificationsController < ApplicationController
   
   def index
-    @notifications = current_user.notifications.order(:created_at, :desc)
+    @notifications = current_user.notifications.order(created_at: :desc)
   end
   
 end
