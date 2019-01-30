81f3f48c7f1675313daada9ca67c1dba52724dc0
Alex Mekelburg <alex@mekware.com>
2019-01-30 16:53:28 -0500

    fix migration for pg

--- db/migrate/20190130195737_add_statuses_to_notifications.rb
+++ db/migrate/20190130195737_add_statuses_to_notifications.rb
@@ -1,9 +1,9 @@
 class AddStatusesToNotifications < ActiveRecord::Migration[5.2]
   def change
     add_column :notifications, :read, :boolean
-    add_column :notifications, :read_at, :date_time
+    add_column :notifications, :read_at, :datetime
     add_column :notifications, :delivered, :boolean
-    add_column :notifications, :delivered_at, :date_time
+    add_column :notifications, :delivered_at, :datetime
     add_column :notifications, :delivery_receipt, :string
   end
 end
