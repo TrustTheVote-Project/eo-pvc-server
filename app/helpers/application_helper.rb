8a47a517279c854ebecc93ba31759febc8e40b43
Alex Mekelburg <alex@mekware.com>
2019-02-23 16:53:08 -0500

    more ui work, build out icons/services list

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,25 +1,42 @@
 module ApplicationHelper
   def notifications_header(back_to = nil)
-    header_block(back_to, 'alert.png', 'notification')    
+    header_block('alert.png', 'notification', back_to)    
   end
   
   def services_header(back_to = nil)
-    header_block(back_to, 'tools.png', 'services')    
+    header_block('tools.png', 'services', back_to)    
+  end
+  
+  def service_header(service_type, service_sub_type, back_to=nil)
+    image = t("services.#{service_type}.icon")
+    key_base = "services.#{service_type}"
+    sub_text = nil
+    if service_sub_type
+      sub_text = t("services.#{service_type}.#{service_sub_type}.header")
+    end
+    
+    header_block(image, key_base, back_to, sub_text)
   end
   
   def settings_header(back_to = nil)
-    header_block(back_to, 'settings.png', 'preferences')
+    header_block('settings.png', 'preferences', back_to)
   end
   
   private
-  def header_block(back_to=nil, image_name, key_base)
+  def header_block(image_name, key_base, back_to=nil, sub_text=nil)
     content_tag(:div, [], class: "header") do
       html = content_tag(:h2, []) do
         html = image_tag(image_name) + t("#{key_base}.header")
       end
+      if sub_text
+        html += content_tag(:p) do
+          sub_text
+        end
+      end
       if back_to
         html += link_to t("#{key_base}.back"), back_to
       end
+      
       html      
     end
     
--- app/models/absentee_request.rb
+++ app/models/absentee_request.rb
@@ -2,11 +2,11 @@ class AbsenteeRequest < ApplicationRecord
   
   belongs_to :user
   
-  validates_presence_of :street_number
-  validates_presence_of :street_name
-  validates_presence_of :city
-  validates_presence_of :postal_code
-  validates_presence_of :email
-  validates_presence_of :gender  
+  # validates_presence_of :street_number
+  # validates_presence_of :street_name
+  # validates_presence_of :city
+  # validates_presence_of :postal_code
+  # validates_presence_of :email
+  # validates_presence_of :gender
   
 end
